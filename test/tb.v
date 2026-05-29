0dc97ff53b740e12776e8c8beed28b2ac285131c
Darshan <61650593+stranger0virus@users.noreply.github.com>
2026-05-29 14:18:05 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_4b_tiny_cpu user_project (
+  tt_um_4b_accumulator_cpu user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
